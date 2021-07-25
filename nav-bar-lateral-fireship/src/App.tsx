9e5717d25e7cecb6d803194dc9f977ea6ed9f450
Victor Adalto <victor.adalto@institutoeducar.com.br>
2021-07-25 03:10:33 -0300

    Commit intermediario

--- nav-bar-lateral-fireship/src/App.tsx
+++ nav-bar-lateral-fireship/src/App.tsx
@@ -1,25 +1,270 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Ball from "../src/types/assets/ball";
+import Dog from "../src/types/assets/dog";
+import Surf from "../src/types/assets/surf";
+import LogoReact from "../src/types/assets/logo-react";
+// import Arrows from "../src/types/assets/arrows";
+import Sun from "./types/assets/sun";
+// import Moon from "../src/types/assets/moon"
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <nav className="navbar">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a href="/#" className="nav-link">
+              <Ball></Ball>
+              <span className="link-text">Bola</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/#" className="nav-link">
+              <Surf></Surf>
+              <span className="link-text">Surfista</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/#" className="nav-link">
+              <Dog></Dog>
+              <span className="link-text">Catioro</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/#" className="nav-link">
+              <LogoReact></LogoReact>
+              <span className="link-text">React</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/#" className="nav-link">
+              <Sun></Sun>
+              <span className="link-text">Tema</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1>Minha Nav Bar de teste!</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, eveniet
+          ut assumenda, nostrum dolore, laudantium nisi enim fugiat voluptatem
+          iure vero nam qui corrupti veritatis earum repudiandae. Iusto, facilis
+          totam. Qui voluptatibus assumenda ex? Illo cupiditate facilis
+          repellendus delectus eveniet hic vitae nostrum voluptate dolore harum
+          maxime rem odit vel ab nisi nobis assumenda voluptatem nemo, velit
+          dolores repudiandae deleniti? Delectus laborum quasi possimus totam
+          deleniti tenetur. Totam, fugiat. Quae, aliquid possimus doloremque
+          mollitia ducimus vel, fugit beatae accusantium esse libero ratione
+          natus dolorem molestiae dignissimos eaque tenetur voluptatum minus.
+          Labore vero tenetur, accusantium saepe quis possimus doloremque id vel
+          iusto unde sapiente accusamus exercitationem ut delectus, voluptatum
+          sunt ullam in tempora obcaecati atque debitis aliquid consequatur.
+          Minus, vitae ipsum! Expedita nisi possimus voluptates eum, ea eius
+          iure rem beatae aspernatur asperiores assumenda. Dicta enim dolore
+          delectus saepe ex necessitatibus perferendis quia? Esse assumenda
+          ipsum cupiditate odit at. Harum, distinctio! Officia ad, excepturi
+          aspernatur amet repellat eaque maiores, quam assumenda laborum
+          exercitationem voluptatum velit ullam pariatur perspiciatis,
+          distinctio accusamus iste. Fugiat magnam laboriosam tempora deserunt
+          adipisci natus obcaecati explicabo blanditiis! Porro, sunt vero minus
+          dolor vel suscipit, similique beatae error quisquam debitis quo
+          deleniti adipisci nesciunt esse, cupiditate totam repellendus iusto?
+          Dolor possimus exercitationem quaerat iure facilis consectetur minus
+          molestiae! Labore dolores enim nemo alias laborum rerum sed optio.
+          Eaque qui unde veritatis deleniti provident temporibus sapiente. Rem
+          dicta accusamus, deleniti neque dolorem nemo numquam cupiditate
+          corrupti molestias voluptas expedita. Sed dolore fuga id, eveniet
+          assumenda alias cum reiciendis recusandae! Quisquam adipisci minima
+          consequuntur rerum labore, cum alias autem ipsam in, impedit accusamus
+          voluptatum voluptates laudantium quas hic vitae provident. Unde, nemo
+          minus dolorum sed consequatur iure distinctio nulla rem dolorem facere
+          iusto animi harum eaque eum a ipsam commodi neque ratione. Commodi
+          modi nulla, odit amet quibusdam temporibus provident.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, eveniet
+          ut assumenda, nostrum dolore, laudantium nisi enim fugiat voluptatem
+          iure vero nam qui corrupti veritatis earum repudiandae. Iusto, facilis
+          totam. Qui voluptatibus assumenda ex? Illo cupiditate facilis
+          repellendus delectus eveniet hic vitae nostrum voluptate dolore harum
+          maxime rem odit vel ab nisi nobis assumenda voluptatem nemo, velit
+          dolores repudiandae deleniti? Delectus laborum quasi possimus totam
+          deleniti tenetur. Totam, fugiat. Quae, aliquid possimus doloremque
+          mollitia ducimus vel, fugit beatae accusantium esse libero ratione
+          natus dolorem molestiae dignissimos eaque tenetur voluptatum minus.
+          Labore vero tenetur, accusantium saepe quis possimus doloremque id vel
+          iusto unde sapiente accusamus exercitationem ut delectus, voluptatum
+          sunt ullam in tempora obcaecati atque debitis aliquid consequatur.
+          Minus, vitae ipsum! Expedita nisi possimus voluptates eum, ea eius
+          iure rem beatae aspernatur asperiores assumenda. Dicta enim dolore
+          delectus saepe ex necessitatibus perferendis quia? Esse assumenda
+          ipsum cupiditate odit at. Harum, distinctio! Officia ad, excepturi
+          aspernatur amet repellat eaque maiores, quam assumenda laborum
+          exercitationem voluptatum velit ullam pariatur perspiciatis,
+          distinctio accusamus iste. Fugiat magnam laboriosam tempora deserunt
+          adipisci natus obcaecati explicabo blanditiis! Porro, sunt vero minus
+          dolor vel suscipit, similique beatae error quisquam debitis quo
+          deleniti adipisci nesciunt esse, cupiditate totam repellendus iusto?
+          Dolor possimus exercitationem quaerat iure facilis consectetur minus
+          molestiae! Labore dolores enim nemo alias laborum rerum sed optio.
+          Eaque qui unde veritatis deleniti provident temporibus sapiente. Rem
+          dicta accusamus, deleniti neque dolorem nemo numquam cupiditate
+          corrupti molestias voluptas expedita. Sed dolore fuga id, eveniet
+          assumenda alias cum reiciendis recusandae! Quisquam adipisci minima
+          consequuntur rerum labore, cum alias autem ipsam in, impedit accusamus
+          voluptatum voluptates laudantium quas hic vitae provident. Unde, nemo
+          minus dolorum sed consequatur iure distinctio nulla rem dolorem facere
+          iusto animi harum eaque eum a ipsam commodi neque ratione. Commodi
+          modi nulla, odit amet quibusdam temporibus provident.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, eveniet
+          ut assumenda, nostrum dolore, laudantium nisi enim fugiat voluptatem
+          iure vero nam qui corrupti veritatis earum repudiandae. Iusto, facilis
+          totam. Qui voluptatibus assumenda ex? Illo cupiditate facilis
+          repellendus delectus eveniet hic vitae nostrum voluptate dolore harum
+          maxime rem odit vel ab nisi nobis assumenda voluptatem nemo, velit
+          dolores repudiandae deleniti? Delectus laborum quasi possimus totam
+          deleniti tenetur. Totam, fugiat. Quae, aliquid possimus doloremque
+          mollitia ducimus vel, fugit beatae accusantium esse libero ratione
+          natus dolorem molestiae dignissimos eaque tenetur voluptatum minus.
+          Labore vero tenetur, accusantium saepe quis possimus doloremque id vel
+          iusto unde sapiente accusamus exercitationem ut delectus, voluptatum
+          sunt ullam in tempora obcaecati atque debitis aliquid consequatur.
+          Minus, vitae ipsum! Expedita nisi possimus voluptates eum, ea eius
+          iure rem beatae aspernatur asperiores assumenda. Dicta enim dolore
+          delectus saepe ex necessitatibus perferendis quia? Esse assumenda
+          ipsum cupiditate odit at. Harum, distinctio! Officia ad, excepturi
+          aspernatur amet repellat eaque maiores, quam assumenda laborum
+          exercitationem voluptatum velit ullam pariatur perspiciatis,
+          distinctio accusamus iste. Fugiat magnam laboriosam tempora deserunt
+          adipisci natus obcaecati explicabo blanditiis! Porro, sunt vero minus
+          dolor vel suscipit, similique beatae error quisquam debitis quo
+          deleniti adipisci nesciunt esse, cupiditate totam repellendus iusto?
+          Dolor possimus exercitationem quaerat iure facilis consectetur minus
+          molestiae! Labore dolores enim nemo alias laborum rerum sed optio.
+          Eaque qui unde veritatis deleniti provident temporibus sapiente. Rem
+          dicta accusamus, deleniti neque dolorem nemo numquam cupiditate
+          corrupti molestias voluptas expedita. Sed dolore fuga id, eveniet
+          assumenda alias cum reiciendis recusandae! Quisquam adipisci minima
+          consequuntur rerum labore, cum alias autem ipsam in, impedit accusamus
+          voluptatum voluptates laudantium quas hic vitae provident. Unde, nemo
+          minus dolorum sed consequatur iure distinctio nulla rem dolorem facere
+          iusto animi harum eaque eum a ipsam commodi neque ratione. Commodi
+          modi nulla, odit amet quibusdam temporibus provident.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, eveniet
+          ut assumenda, nostrum dolore, laudantium nisi enim fugiat voluptatem
+          iure vero nam qui corrupti veritatis earum repudiandae. Iusto, facilis
+          totam. Qui voluptatibus assumenda ex? Illo cupiditate facilis
+          repellendus delectus eveniet hic vitae nostrum voluptate dolore harum
+          maxime rem odit vel ab nisi nobis assumenda voluptatem nemo, velit
+          dolores repudiandae deleniti? Delectus laborum quasi possimus totam
+          deleniti tenetur. Totam, fugiat. Quae, aliquid possimus doloremque
+          mollitia ducimus vel, fugit beatae accusantium esse libero ratione
+          natus dolorem molestiae dignissimos eaque tenetur voluptatum minus.
+          Labore vero tenetur, accusantium saepe quis possimus doloremque id vel
+          iusto unde sapiente accusamus exercitationem ut delectus, voluptatum
+          sunt ullam in tempora obcaecati atque debitis aliquid consequatur.
+          Minus, vitae ipsum! Expedita nisi possimus voluptates eum, ea eius
+          iure rem beatae aspernatur asperiores assumenda. Dicta enim dolore
+          delectus saepe ex necessitatibus perferendis quia? Esse assumenda
+          ipsum cupiditate odit at. Harum, distinctio! Officia ad, excepturi
+          aspernatur amet repellat eaque maiores, quam assumenda laborum
+          exercitationem voluptatum velit ullam pariatur perspiciatis,
+          distinctio accusamus iste. Fugiat magnam laboriosam tempora deserunt
+          adipisci natus obcaecati explicabo blanditiis! Porro, sunt vero minus
+          dolor vel suscipit, similique beatae error quisquam debitis quo
+          deleniti adipisci nesciunt esse, cupiditate totam repellendus iusto?
+          Dolor possimus exercitationem quaerat iure facilis consectetur minus
+          molestiae! Labore dolores enim nemo alias laborum rerum sed optio.
+          Eaque qui unde veritatis deleniti provident temporibus sapiente. Rem
+          dicta accusamus, deleniti neque dolorem nemo numquam cupiditate
+          corrupti molestias voluptas expedita. Sed dolore fuga id, eveniet
+          assumenda alias cum reiciendis recusandae! Quisquam adipisci minima
+          consequuntur rerum labore, cum alias autem ipsam in, impedit accusamus
+          voluptatum voluptates laudantium quas hic vitae provident. Unde, nemo
+          minus dolorum sed consequatur iure distinctio nulla rem dolorem facere
+          iusto animi harum eaque eum a ipsam commodi neque ratione. Commodi
+          modi nulla, odit amet quibusdam temporibus provident.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, eveniet
+          ut assumenda, nostrum dolore, laudantium nisi enim fugiat voluptatem
+          iure vero nam qui corrupti veritatis earum repudiandae. Iusto, facilis
+          totam. Qui voluptatibus assumenda ex? Illo cupiditate facilis
+          repellendus delectus eveniet hic vitae nostrum voluptate dolore harum
+          maxime rem odit vel ab nisi nobis assumenda voluptatem nemo, velit
+          dolores repudiandae deleniti? Delectus laborum quasi possimus totam
+          deleniti tenetur. Totam, fugiat. Quae, aliquid possimus doloremque
+          mollitia ducimus vel, fugit beatae accusantium esse libero ratione
+          natus dolorem molestiae dignissimos eaque tenetur voluptatum minus.
+          Labore vero tenetur, accusantium saepe quis possimus doloremque id vel
+          iusto unde sapiente accusamus exercitationem ut delectus, voluptatum
+          sunt ullam in tempora obcaecati atque debitis aliquid consequatur.
+          Minus, vitae ipsum! Expedita nisi possimus voluptates eum, ea eius
+          iure rem beatae aspernatur asperiores assumenda. Dicta enim dolore
+          delectus saepe ex necessitatibus perferendis quia? Esse assumenda
+          ipsum cupiditate odit at. Harum, distinctio! Officia ad, excepturi
+          aspernatur amet repellat eaque maiores, quam assumenda laborum
+          exercitationem voluptatum velit ullam pariatur perspiciatis,
+          distinctio accusamus iste. Fugiat magnam laboriosam tempora deserunt
+          adipisci natus obcaecati explicabo blanditiis! Porro, sunt vero minus
+          dolor vel suscipit, similique beatae error quisquam debitis quo
+          deleniti adipisci nesciunt esse, cupiditate totam repellendus iusto?
+          Dolor possimus exercitationem quaerat iure facilis consectetur minus
+          molestiae! Labore dolores enim nemo alias laborum rerum sed optio.
+          Eaque qui unde veritatis deleniti provident temporibus sapiente. Rem
+          dicta accusamus, deleniti neque dolorem nemo numquam cupiditate
+          corrupti molestias voluptas expedita. Sed dolore fuga id, eveniet
+          assumenda alias cum reiciendis recusandae! Quisquam adipisci minima
+          consequuntur rerum labore, cum alias autem ipsam in, impedit accusamus
+          voluptatum voluptates laudantium quas hic vitae provident. Unde, nemo
+          minus dolorum sed consequatur iure distinctio nulla rem dolorem facere
+          iusto animi harum eaque eum a ipsam commodi neque ratione. Commodi
+          modi nulla, odit amet quibusdam temporibus provident.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, eveniet
+          ut assumenda, nostrum dolore, laudantium nisi enim fugiat voluptatem
+          iure vero nam qui corrupti veritatis earum repudiandae. Iusto, facilis
+          totam. Qui voluptatibus assumenda ex? Illo cupiditate facilis
+          repellendus delectus eveniet hic vitae nostrum voluptate dolore harum
+          maxime rem odit vel ab nisi nobis assumenda voluptatem nemo, velit
+          dolores repudiandae deleniti? Delectus laborum quasi possimus totam
+          deleniti tenetur. Totam, fugiat. Quae, aliquid possimus doloremque
+          mollitia ducimus vel, fugit beatae accusantium esse libero ratione
+          natus dolorem molestiae dignissimos eaque tenetur voluptatum minus.
+          Labore vero tenetur, accusantium saepe quis possimus doloremque id vel
+          iusto unde sapiente accusamus exercitationem ut delectus, voluptatum
+          sunt ullam in tempora obcaecati atque debitis aliquid consequatur.
+          Minus, vitae ipsum! Expedita nisi possimus voluptates eum, ea eius
+          iure rem beatae aspernatur asperiores assumenda. Dicta enim dolore
+          delectus saepe ex necessitatibus perferendis quia? Esse assumenda
+          ipsum cupiditate odit at. Harum, distinctio! Officia ad, excepturi
+          aspernatur amet repellat eaque maiores, quam assumenda laborum
+          exercitationem voluptatum velit ullam pariatur perspiciatis,
+          distinctio accusamus iste. Fugiat magnam laboriosam tempora deserunt
+          adipisci natus obcaecati explicabo blanditiis! Porro, sunt vero minus
+          dolor vel suscipit, similique beatae error quisquam debitis quo
+          deleniti adipisci nesciunt esse, cupiditate totam repellendus iusto?
+          Dolor possimus exercitationem quaerat iure facilis consectetur minus
+          molestiae! Labore dolores enim nemo alias laborum rerum sed optio.
+          Eaque qui unde veritatis deleniti provident temporibus sapiente. Rem
+          dicta accusamus, deleniti neque dolorem nemo numquam cupiditate
+          corrupti molestias voluptas expedita. Sed dolore fuga id, eveniet
+          assumenda alias cum reiciendis recusandae! Quisquam adipisci minima
+          consequuntur rerum labore, cum alias autem ipsam in, impedit accusamus
+          voluptatum voluptates laudantium quas hic vitae provident. Unde, nemo
+          minus dolorum sed consequatur iure distinctio nulla rem dolorem facere
+          iusto animi harum eaque eum a ipsam commodi neque ratione. Commodi
+          modi nulla, odit amet quibusdam temporibus provident.
+        </p>
+      </main>
+    </>
   );
 }
 
